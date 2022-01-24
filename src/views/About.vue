97f612abbee3ba1d185874e0b3d56a42c27f3911
Alex Bates <alex@hciyork.com>
2022-01-24 18:08:38 +0000

    more changes to animation and content

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,11 @@
   <div id="about" class="mx-auto w-full max-w-screen-xl">
     <div id="hero" class="mx-auto max-w-screen-xl py-10">
       <div class="hero-title dark:text-white text-pastel-blue font-bold text-5xl mb-5">About me</div>
+      <div class="hero-subtitle dark:text-gray-400 text-black-700 text-xl">What interests me in and out of work.</div>
+      <p class="text-lg text-white mt-20 my-5">I've been developing websites and front-end applications since 2014, after I graduated from a Music course at University. I didn't have any prior experience but took it upon myself to learn the basics of HTML, CSS &amp; Javascript. From there I've never stopped learning and try to always keep up to date with the latest technologies and also what interests me.</p>
+      <p class="text-lg text-white my-5">Through my current employment I've been exposed to Javascript frameworks and libraries such as Vue &amp; React. I've also written large-scale applications in C# Asp.net and helped develop Web Apis using .NET Core.</p>
+      <p class="text-lg text-white my-5">I've got experience with CI/CD, having helped bring our company up-to-date with how we deploy our work.</p>
+      <p class="text-lg text-white my-5">Outside of work, I'm passionate about Walking &amp; playing Music. Most weekends are taken up with going on long adventures with my rescue dog, Zafi.</p>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <div id="hero" class="mx-auto py-10">
       <div class="hero-title dark:text-white text-pastel-blue font-bold text-5xl mb-5">Hi, I'm Alex</div>
       <div class="hero-subtitle dark:text-gray-400 text-black-700 text-xl">I'm a front-end developer, musician and dog-owner.</div>
-      <div class="hero-bullets mt-20 my-10">
-        <div v-for="(bullet, idx) in bullets" :key="idx" class="hero-bullet flex items-center my-5">
+      <div class="hero-bullets mt-20 my-10 ">
+        <div v-for="(bullet, idx) in bullets" :key="idx" class="hero-bullet flex items-center my-5 animate__animated animate__faster animate__fadeInUp" :class="`animate__delay-${bullet.delay}`">
           <div class="hero-bullet-icon flex-none w-auto pr-10" v-html="bullet.icon"></div>
           <div class="hero-bullet-text dark:text-white text-gray-700 text-lg">{{bullet.content}}</div>
         </div>
@@ -26,15 +26,18 @@ export default defineComponent({
       bullets: [
         {
           icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 dark:text-white text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" /></svg>',
-          content: "I'm a Javascript developer first and foremost. Having built multiple large and small scale applications using different Javascript frameworks and libraries, I'm comfortable adapting to whatever modern environment I find myself working in."
+          content: "I'm a Javascript developer first and foremost. Having built multiple large and small scale applications using different Javascript frameworks and libraries, I'm comfortable adapting to whatever modern environment I find myself working in.",
+          delay: '1s'
         },
         {
           icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 dark:text-white text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>',
           content: "I'm comfortable using Node, NPM and general terminal manipulation.",
+          delay: '2s'
         },
         {
           icon: '<svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 dark:text-white text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /></svg>',
           content: "I can work well in both large and small teams, and also enjoy working solo when required.",
+          delay: '3s'
         }
       ]
     }
@@ -43,4 +46,7 @@ export default defineComponent({
 </script>
 
 <style lang="less">
+.hero-bullet {
+  --animate-delay: 0.25s;
+}
 </style>
