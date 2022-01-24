80669d386d14f8bb8110464fd96d408be9ec3f2a
Alex Bates <alex@hciyork.com>
2022-01-24 22:11:55 +0000

    start adding some vue3/typescript specific elements

---
+++ src/components/portfolio/PortfolioItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <article @click="toggleActive" class="p-5 rounded-md dark:bg-gray-900 bg-gray-100 animate__animated animate__faster animate__fadeInUp animate__delay-1s" id="github">
+    <div class="text-white text-2xl font-bold mb-5">{{title}}</div>
+    <div class="dark:text-gray-400 text-black-700 text-lg mb-10">{{summary}}</div>
+    <a :href="link" class="sm:w-auto text-lg font-semibold bg-pastel-blue rounded-lg text-white py-2 px-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-900 hover:bg-violet-500">View</a>
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String,
+    summary: String,
+    link: String
+  },
+  setup(props) {
+    const active = ref(false)
+
+    return { 
+      active 
+    }
+  },
+  methods: {
+    toggleActive() {
+      this.active = !this.active
+    }
+  }
+})
+</script>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -4,7 +4,9 @@
       <div class="hero-title text-white font-bold text-5xl mb-5">My work</div>
       <div class="hero-subtitle dark:text-gray-400 text-black-700 text-xl">A few examples of what I get up to both in work and play.</div>
       <section id="workGrid" class="mt-20 grid gap-5 grid-cols-2">
-        <article class="p-5 rounded-md dark:bg-gray-900 bg-gray-100 animate__animated animate__faster animate__fadeInUp animate__delay-1s" id="github">
+        <PortfolioItem v-for="(item, idx) in workItems" :key="idx" 
+          :title="item.title" :summary="item.summary" :link="item.link" />
+        <!-- <article class="p-5 rounded-md dark:bg-gray-900 bg-gray-100 animate__animated animate__faster animate__fadeInUp animate__delay-1s" id="github">
           <div class="text-white text-2xl font-bold mb-5">Github</div>
           <div class="dark:text-gray-400 text-black-700 text-lg mb-10">text about title.</div>
           <a href="https://github.com/ajbates93" class="sm:w-auto text-lg font-semibold bg-pastel-blue rounded-lg text-white py-2 px-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-900 hover:bg-violet-500">View</a>
@@ -23,11 +25,61 @@
           <div class="text-white text-2xl font-bold mb-5">Github</div>
           <div class="dark:text-gray-400 text-black-700 text-lg mb-10">text about title.</div>
           <a href="https://github.com/ajbates93" class="sm:w-auto text-lg font-semibold bg-pastel-blue rounded-lg text-white py-2 px-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-violet-600 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-900 hover:bg-violet-500">View</a>
-        </article>
+        </article> -->
       </section>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import PortfolioItem from '../components/portfolio/PortfolioItem.vue'
+
+interface itemStructure {
+  title: string,
+  summary: string,
+  link: string
+}
+
+const items: itemStructure[] = [
+  {
+    title: 'Github',
+    summary: 'text about title.',
+    link: 'https://github.com/ajbates93'
+  },
+  {
+    title: 'Github',
+    summary: 'text about title.',
+    link: 'https://github.com/ajbates93'
+  },
+  {
+    title: 'Github',
+    summary: 'text about title.',
+    link: 'https://github.com/ajbates93'
+  },
+  {
+    title: 'Github',
+    summary: 'text about title.',
+    link: 'https://github.com/ajbates93'
+  }
+]
+
+export default defineComponent({
+  setup() {
+    const workItems: itemStructure[] = ref([])
+    items.forEach(x => workItems.value.push(x))
+    workItems.value.push(items)
+    return workItems
+  },
+  methods: {
+
+  },
+  components: {
+    PortfolioItem
+  }
+})
+</script>
+
 
 <style lang="less">
 #portfolio {
